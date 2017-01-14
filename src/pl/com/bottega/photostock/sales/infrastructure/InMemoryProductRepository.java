b4891ee790df911561fb678e91908cfbf7cbb881
macieydudek <macieydudek@gmail.com>
2017-01-14 15:37:18 +0100

    search available

--- src/pl/com/bottega/photostock/sales/infrastructure/InMemoryProductRepository.java
+++ src/pl/com/bottega/photostock/sales/infrastructure/InMemoryProductRepository.java
@@ -50,11 +50,16 @@ public class InMemoryProductRepository implements ProductRepository {
         return matchingProducts;
     }
 
-    private boolean matches(Client client, Product product, String nameQuery, String[] tags, Money priceFrom, Money priceTo, boolean onlyActive) {
+    private boolean matches(Client client, Product product, String nameQuery, String[] tags, Money priceFrom, Money priceTo, boolean onlyAvailable) {
         return matchesQuery(product, nameQuery)
                 && matchesTags(product, tags)
                 && matchesPriceFrom(client, product, priceFrom)
-                && matchesPriceTo(client, product, priceTo);
+                && matchesPriceTo(client, product, priceTo)
+                && matchesOnlyActive(product, onlyAvailable);
+    }
+
+    private boolean matchesOnlyActive(Product product, boolean onlyAvailable) {
+        return !onlyAvailable || product.isAvailable();
     }
 
     private boolean matchesPriceTo(Client client, Product product, Money priceTo) {
